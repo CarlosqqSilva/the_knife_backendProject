94e4c485df0a9fcd345e22422ace9d8646d56b79
mtsluis <151209805+mtsluis@users.noreply.github.com>
2024-01-23 10:21:09 +0000

    added project

---
+++ src/main/java/org/mindswap/springtheknife/SpringTheKnifeApplication.java
@@ -0,0 +1,13 @@
+package org.mindswap.springtheknife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTheKnifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTheKnifeApplication.class, args);
+	}
+
+}
