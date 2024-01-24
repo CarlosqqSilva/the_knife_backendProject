4e4efe2f687f0fb1e7acd15c20cf8c5f7e058bcc
barbara-rolim <barbaraerolim@gmail.com>
2024-01-24 19:16:22 +0000

    commit create_city

---
+++ src/main/java/org/mindswap/springtheknife/utilities/Messages.java
@@ -0,0 +1,9 @@
+package org.mindswap.springtheknife.utilities;
+
+public class Messages {
+    public static final String CITY_WITH_ID = "City with id";
+    public static final String NOT_EXIST = "does not exist";
+    public static final String DUPLICATE_NAME ="City with name";
+    public static final String EXIST = "already exists";
+    public static final String NAME_ERROR = "Must have name";
+}
