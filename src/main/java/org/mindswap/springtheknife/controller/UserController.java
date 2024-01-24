6d1ac44b970d709c094d8714b277ef4da40e3859
pedrosoares7 <soarespedro7@hotmail.com>
2024-01-24 12:32:10 +0000

    add user aspects - loggingAspect

--- src/main/java/org/mindswap/springtheknife/controller/UserController.java
+++ src/main/java/org/mindswap/springtheknife/controller/UserController.java
@@ -17,7 +17,6 @@ import org.mindswap.springtheknife.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
