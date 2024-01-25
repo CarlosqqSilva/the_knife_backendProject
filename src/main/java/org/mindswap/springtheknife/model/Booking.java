94941672ed2594992ca77b930f0206dc961edc59
barbara-rolim <barbaraerolim@gmail.com>
2024-01-25 15:54:35 +0000

    commit create_booking

---
+++ src/main/java/org/mindswap/springtheknife/model/Booking.java
@@ -0,0 +1,35 @@
+package org.mindswap.springtheknife.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.mindswap.springtheknife.Enum.BookingStatus;
+import org.mindswap.springtheknife.utils.Message;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "booking")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull(message = Message.BOOKING_MANDATORY)
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime bookingTime;
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+}
