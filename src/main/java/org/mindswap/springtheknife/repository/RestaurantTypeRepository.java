94886fca76d0c39dfae9e1d5d5f8359c8d3af650
barbara-rolim <barbaraerolim@gmail.com>
2024-02-01 15:18:38 +0000

    commit -restaurantType-Test

--- src/main/java/org/mindswap/springtheknife/repository/RestaurantTypeRepository.java
+++ src/main/java/org/mindswap/springtheknife/repository/RestaurantTypeRepository.java
@@ -1,10 +1,19 @@
 package org.mindswap.springtheknife.repository;
 
+import jakarta.transaction.Transactional;
 import org.mindswap.springtheknife.model.RestaurantType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface RestaurantTypeRepository extends JpaRepository<RestaurantType, Long> {
     Optional<RestaurantType> findByType(String type);
+
+    @Modifying
+    @Transactional
+    @Query(value = "ALTER TABLE restaurant_type AUTO_INCREMENT = 1", nativeQuery = true)
+    default void resetId() {
+    }
 }
